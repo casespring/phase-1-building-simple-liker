1d9be60e03334487788b09c9494e4a7a815f5e26
Marco Upia <markremembers@gmail.com>
2023-11-09 09:53:12 -0500

    not finished

--- main.js
+++ main.js
@@ -3,15 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+mimicServerCall(addEventListener("click", e => {
+    console.log(e.target);
+    let fillHeart = document.getElementsByTagName('span')
+    fillHeart.textContent = FULL_HEART;
+    fillHeart.
 
+})).then(data => console.log(data))
+.catch(e => {
+  
+  let getModal = document.getElementById('modal')
+  let getErrorP = document.getElementById('modal-message')
+  getModal.classList.remove('hidden');
+  getErrorP.textContent = e.message;
+  setTimeout(() => {
 
+    getModal.classList.add("hidden")
+  }, "3000");
+
+})
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(url="http://mimicServer.example.com/", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
